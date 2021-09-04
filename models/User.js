955fd8f4627abea1036732acfe7cf3eb6a8be026
VINAY R NITTALI <vinayvrn12@gmail.com>
2021-09-04 20:27:04 +0530

    Added signup and a user model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: 8
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
