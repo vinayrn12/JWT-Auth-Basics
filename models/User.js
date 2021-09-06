fc1d0848c386ab062ac4a068f22c0f40ed6c78b9
VINAY R NITTALI <vinayvrn12@gmail.com>
2021-09-06 20:20:39 +0530

    Added bcrpt hashing for securing passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,22 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter a password'],
         minlength: [8, 'The password must contain atleast 8 characters']
     }
+});
+
+//Mongoose hooks 
+
+//This function is fired after a new instance is saved to the db
+// userSchema.post('save', function(doc, next){
+//     console.log('User created and saved.', doc);
+//     next(); //Pass to the next section bcoz this is a middleware
+// });
+
+//This function is fired before a new instance is saved to the db
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next(); //Pass to the next section bcoz this is a middleware
 });
 
 const User = mongoose.model('user', userSchema);
